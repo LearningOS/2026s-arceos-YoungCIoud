304fea362db758b6566c051a1bd6d0262ac6972e
YoungCIoud <YOUNG.050330.cdut@outlook.com>
2026-04-14 20:03:00 +0800

    同时输出有颜色的文本和无颜色的文本

--- arceos/exercises/print_with_color/src/main.rs
+++ arceos/exercises/print_with_color/src/main.rs
@@ -2,9 +2,10 @@
 #![cfg_attr(feature = "axstd", no_main)]
 
 #[cfg(feature = "axstd")]
-use axstd::colorln;
+use axstd::{colorln, println};
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     colorln!("[WithColor]: Hello, Arceos!");
+    println!("[WithoutColor]: Hello, Arceos!");
 }
